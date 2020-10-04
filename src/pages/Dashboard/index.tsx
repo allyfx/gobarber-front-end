ed52f6650b21dc6d537222028dbaa98925dd458f
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-04 00:59:08 -0300

    Update profile page added

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,6 +4,8 @@ import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
+import { Link } from 'react-router-dom';
+
 import { FiPower, FiClock } from 'react-icons/fi';
 
 import {
@@ -143,7 +145,9 @@ const Dashboard: React.FC = () => {
                         <img src={user.avatar_url} alt={user.name} />
                         <div>
                             <span>Bem-vindo,</span>
-                            <strong>{user.name}</strong>
+                            <Link to="/profile">
+                                <strong>{user.name}</strong>
+                            </Link>
                         </div>
                     </Profile>
 
